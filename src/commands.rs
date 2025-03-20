d5c35762ac415f8fe674424ce6517698a8b55e74
Akif Tunc <akiftuncc@outlook.com>
2025-03-20 15:16:52 +0300

    assign roles to users

--- src/commands.rs
+++ src/commands.rs
@@ -1,6 +1,6 @@
 use diesel_async::{AsyncConnection, AsyncPgConnection};
 
-use crate::{repositories::UserRepository, models::NewUser};
+use crate::{models::NewUser, repositories::{RoleRepository, UserRepository}};
 
 async fn load_db_connection() -> AsyncPgConnection{
     let database_url = std::env::var("DATABASE_URL")
@@ -14,8 +14,10 @@ pub async fn create_user(username:String,password:String, role_codes: Vec<String
     let mut c = load_db_connection().await;
 
     let new_user = NewUser {username,password};
-    let user = UserRepository::create(&mut c,new_user).await.unwrap();
+    let user = UserRepository::create(&mut c,new_user, role_codes).await.unwrap();
     println!("User created: {:?}",user);
+    let roles = RoleRepository::find_by_user(&mut c, &user).await.unwrap();
+    print!("Roles assigned {:?}",roles);
 }
 
 pub async fn list_users() {
