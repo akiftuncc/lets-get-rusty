616431f5f8fd55fc1220e9ecff493ef112c64c89
Akif Tunc <akiftuncc@outlook.com>
2025-03-20 16:24:24 +0300

    add passoword hashing

--- src/commands.rs
+++ src/commands.rs
@@ -1,3 +1,5 @@
+use argon2::PasswordHasher;
+use argon2::password_hash::{rand_core::OsRng, SaltString};
 use diesel_async::{AsyncConnection, AsyncPgConnection};
 
 use crate::{models::NewUser, repositories::{RoleRepository, UserRepository}};
@@ -13,7 +15,11 @@ async fn load_db_connection() -> AsyncPgConnection{
 pub async fn create_user(username:String,password:String, role_codes: Vec<String>) {
     let mut c = load_db_connection().await;
 
-    let new_user = NewUser {username,password};
+    let salt = SaltString::generate(OsRng);
+    let argon2 = argon2::Argon2::default();
+    let password_hash = argon2.hash_password(password.as_bytes(), &salt).unwrap();
+
+    let new_user = NewUser {username,password:password_hash.to_string()};
     let user = UserRepository::create(&mut c,new_user, role_codes).await.unwrap();
     println!("User created: {:?}",user);
     let roles = RoleRepository::find_by_user(&mut c, &user).await.unwrap();
